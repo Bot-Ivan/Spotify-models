e4882960f7a8fb324ac5cdd3ed062d3fe51d2471
IvanF <60158053+Bot-Ivan@users.noreply.github.com>
2023-09-16 14:44:34 -0400

    Finished the trendOVerYears function
    This function takes a dataframe and a factor to measure as arguments. Then it creates a line chart showing the changes of the factor_to_measure by the years

--- Spotify data visualization.py
+++ Spotify data visualization.py
@@ -1,15 +1,29 @@
 #spotify stuff
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
-initial_dataframe = pd.read_csv("SpotifyAllTime.csv")
+def trendOverYears(dataframe, factor_to_measure):
+    trend_by_year = dataframe.groupby('Year')[factor_to_measure].mean().reset_index()
+    print(trend_by_year)
+    plt.figure(figsize = (10, 6))
+    plt.plot(trend_by_year["Year"], trend_by_year[factor_to_measure], marker = "o", linestyle = "-")
+    plt.title("Danceability Trend over the years")
+    plt.xlabel("Year")
+    plt.ylabel("Average Danceability")
+    plt.grid(True)
+    plt.show()
+
+    
+
+initial_spotify_dataframe = pd.read_csv("SpotifyAllTime.csv")
 # print (initial_dataframe)
 # print (initial_dataframe.columns)
 
 #data visualization
-
+print("For what factor would you like to see the changes over the years? ", initial_spotify_dataframe.columns)
+trendOverYears(initial_spotify_dataframe, "Danceability")
 
 
 # scikit-learn
 # regression for song popularity
-# classification for genre
+# classification for genres
