28770e7082ba1ebae40648d634547930a2533b57
IvanF <60158053+Bot-Ivan@users.noreply.github.com>
2023-09-16 13:53:49 -0400

    Initial main file

---
+++ Spotify data visualization.py
@@ -0,0 +1,15 @@
+#spotify stuff
+import pandas as pd
+import matplotlib as plt
+
+initial_dataframe = pd.read_csv("SpotifyAllTime.csv")
+# print (initial_dataframe)
+# print (initial_dataframe.columns)
+
+#data visualization
+
+
+
+# scikit-learn
+# regression for song popularity
+# classification for genre
