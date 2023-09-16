028e0c1f4791e8e861bdcfb4b135a4e3ffa82a23
IvanF <60158053+Bot-Ivan@users.noreply.github.com>
2023-09-16 18:48:17 -0400

    updated trendOverYears and imported tkinter
    When prompting the user to choose a genre, it will list the genres from most to least popular.

--- Spotify data visualization.py
+++ Spotify data visualization.py
@@ -1,18 +1,23 @@
 #spotify stuff
 import pandas as pd
 import matplotlib.pyplot as plt
+import tkinter as tk
 
-def trendOverYears(dataframe, factor_to_measure):
+
+
+def trendOverYears(dataframe, factor_to_measure): #Currently not working with Length (Duration)
     if factor_to_measure == "Top Genre":
+        genres_counts = dataframe["Top Genre"].value_counts()
+        genres = genres_counts.index.tolist()
+        print (genres)
         genre = input("Which genre would you like to visualize over time?\n")
         dataframe = dataframe[dataframe['Top Genre'] == genre]
         trend_by_year = dataframe.groupby("Year")["Popularity"].mean().reset_index()
-        print(trend_by_year)
         plt.figure(figsize = (10, 6))
         plt.plot(trend_by_year["Year"], trend_by_year["Popularity"], marker = "o", linestyle = "-")
         plt.title(f"{genre} over the years")
         plt.xlabel("Year")
-        plt.ylabel("Popularity")
+        plt.ylabel("Popularity")        
         plt.grid(True)
         plt.show()
     else:
@@ -26,18 +31,14 @@ def trendOverYears(dataframe, factor_to_measure):
         plt.show()
 
 initial_spotify_dataframe = pd.read_csv("SpotifyAllTime.csv")
-# print (initial_dataframe)
-# print (initial_dataframe.columns)
 
-#data visualization
-features = list(initial_spotify_dataframe.columns[5:-1])
-features.insert(0, "Top Genre")
-
-print("For what factor would you like to see the changes over the years? \n",  features)
-factor_to_measure = input()
-trendOverYears(initial_spotify_dataframe, factor_to_measure)
-
-
-# scikit-learn
-# regression for song popularity
-# classification for genres
+root = tk.Tk()
+root.title("Spotify")
+root.geometry("800x800")
+root.mainloop()
+# data visualization
+# features = list(initial_spotify_dataframe.columns[5:-1])
+# features.insert(0, "Top Genre")
+# print("For what factor would you like to see the changes over the years? \n",  features)
+# factor_to_measure = input()
+# trendOverYears(initial_spotify_dataframe, factor_to_measure)
