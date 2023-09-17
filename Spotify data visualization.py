907769beb5b0f8f19baf509775690ca1f1d6daf5
Cate <catherinemmcginnis@gmail.com>
2023-09-17 10:45:21 -0400

    added graphs 🔥💯

--- Spotify data visualization.py
+++ Spotify data visualization.py
@@ -1,7 +1,7 @@
 #spotify stuff
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
@@ -14,9 +14,6 @@ x = initial_dataframe.drop("top genre", axis=1)
 # df of targets
 y = initial_dataframe["top genre"]
 
-
-avg_score = 0.0
-n = 30
         
 # splitting of data
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
@@ -38,13 +35,10 @@ model.fit(x_train, y_train)
 
 #storing the predictions made from the trained model
 y_pred = model.predict(x_test) 
-
 y_pred = y_pred.flatten()
 
 predicted_genre = []
 numeric_labels = list(genre_to_label.values())
-
-# y_pred = list(y_pred)
 
 for num_pred in y_pred:
     for label in numeric_labels:
@@ -52,20 +46,34 @@ for num_pred in y_pred:
             # Find the corresponding genre for the numeric label
             predicted_genre.append(next((genre for genre, l in genre_to_label.items() if l == label), None))
 
-# Now, predicted_genre contains the corresponding genres for the predicted labels
 print(predicted_genre)
 
-            
-    # # Map each integer label to the corresponding genre string
-    # predicted_genre = [genre for genre, label in genre_to_label.items() if label == num_pred]
+# Create histograms for original numeric columns
+plot_df = x_train.select_dtypes(include=['int64', 'float64', 'bool'])
 
-    # if predicted_genre:
-    #     # If there is a predicted genre (non-empty list), print the first one
-    #     print("Predicted Genre:", predicted_genre[0])
-    # else:
-    #     # Handle the case when there is no matching genre label
-    #     print("No matching genre found for label:", num_pred)
+for column in plot_df.columns:
+    # Define data
+    data = x_train[column]
+    bins = np.linspace(min(data), max(data), 20)
 
-# Calculate accuracy
-#accuracy = accuracy_score(y_test, y_pred)
+    # Create histogram
+    plt.hist(data, bins=bins, alpha=0.5, color='green')
+
+    # Add labels and title
+    plt.xlabel(column)
+    plt.ylabel('Count')
+    plt.title(f'Distribution of {column}')
+    plt.show()
+
+# Slice the first 100 elements of y_pred
+y_pred_subset = y_pred[:50]
+
+# Create a histogram for the limited subset of predicted genres
+plt.hist(y_pred_subset, bins=len(unique_genres))  # Use the number of unique genres as bins
+plt.xlabel('Predicted Genre Label')
+plt.ylabel('Frequency')
+plt.title('Histogram of Predicted Genres (First 100)')
+plt.xticks(range(len(unique_genres)), unique_genres, rotation=90)  # Assign genre names to x-ticks
+plt.show()
+
 
